196b63982386a31550254ef04f7e6c0d9ea10fed
Jorge <curruscataphractus@gmail.com>
2011-03-30 21:37:45 +0200

    Initial import

---
+++ bikes/admin.py
@@ -0,0 +1,9 @@
+
+from bikes.models import Brand, Model, Client, Invoice, Bike
+from django.contrib import admin
+
+admin.site.register(Brand)
+admin.site.register(Model)
+admin.site.register(Client)
+admin.site.register(Invoice)
+admin.site.register(Bike)
